4ee07da204265135fe79f669d8bcc57ee27a2647
Maximilian Stüber <Maximilian.Stueber@me.com>
2015-10-12 20:06:04 +0200

    Further tests

--- mdm_core/mdm_view/src/main/webapp/components/overview/Component.js
+++ mdm_core/mdm_view/src/main/webapp/components/overview/Component.js
@@ -1,22 +1,24 @@
 sap.ui.define([ 'sap/ui/core/UIComponent' ], function(UIComponent) {
 	"use strict";
 
-	var Component = UIComponent.extend("uni.mannheim.mdm.components.overview.Component", {
-
-		metadata : {
-			rootView : "uni.mannheim.mdm.components.overview.Overview",
-			dependencies : {
-				libs : [ "sap.suite.ui.commons" ]
-			},
-			config : {
-				sample : {
-					stretch : true,
-					files : [ "Overview.view.xml", "Overview.controller.js", "data.json" ]
+	var Component = UIComponent.extend(
+			"uni.mannheim.mdm.components.overview.Component", {
+				metadata : {
+					rootView : "uni.mannheim.mdm.components.overview.Overview",
+					dependencies : {
+						libs : [ "sap.suite.ui.commons" ]
+					},
+					config : {
+						sample : {
+							stretch : true,
+							files : [
+							        "Overview.view.xml",
+									"Overview.controller.js"
+							]
+						}
+					}
 				}
-			}
-		}
-	});
+			});
 
 	return Component;
-
 });
