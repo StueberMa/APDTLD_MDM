50f9f9aaca0deb8b9138ec97e44119c73f44b9fd
Eva <eva@gje.ch>
2015-10-15 19:58:17 +0200

    Added product class (incl. associated procurement) to the model

---
+++ mdm_core/mdm_model/src/main/java/uni/mannheim/apdtld/mdm_model/persistence/Product.java
@@ -0,0 +1,125 @@
+package uni.mannheim.apdtld.mdm_model.persistence;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+/**
+ * Entity Product.
+ * 
+ * @author APDTLD_MDM @ Uni Mannheim
+ * @version 15.10.2015
+ */
+@Entity
+public class Product implements Serializable {
+	// constants
+	private static final long serialVersionUID = 1L;
+
+	// attributes
+	@Id
+	@GeneratedValue
+	private int id;
+
+	private String name;
+	private String description;
+	private String unitOfMeasure;
+	private int grossWeight;
+	private int netWeight;
+	private String weightUnit;
+	private String size;
+	private String color;
+	private int price;
+	private String currency;
+	private Procurement procurement;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String param) {
+		this.name = param;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String param) {
+		this.description = param;
+	}
+
+	public String getUnitOfMeasure() {
+		return unitOfMeasure;
+	}
+
+	public void setUnitOfMeasure(String param) {
+		this.unitOfMeasure = param;
+	}
+
+	public int getGrossWeight() {
+		return grossWeight;
+	}
+
+	public void setGrossWeight(int param) {
+		this.grossWeight = param;
+	}
+
+	public int getNetWeight() {
+		return netWeight;
+	}
+
+	public void setNetWeight(int param) {
+		this.netWeight = param;
+	}
+
+	public String getWeightUnit() {
+		return weightUnit;
+	}
+
+	public void setWeightUnit(String param) {
+		this.weightUnit = param;
+	}
+
+	public String getSize() {
+		return size;
+	}
+
+	public void setSize(String param) {
+		this.size = param;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String param) {
+		this.color = param;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int param) {
+		this.price = param;
+	}
+
+	public String getCurrency() {
+		return currency;
+	}
+
+	public void setCurrency(String param) {
+		this.currency = param;
+	}
+
+	public Procurement getProcurement() {
+		return procurement;
+	}
+
+	public void setProcurement(Procurement param) {
+		this.procurement = param;
+	}
+
+}
