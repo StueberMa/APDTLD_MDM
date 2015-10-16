98a008a7352f81aa0bd3be7b0f2717413737c10b
Maximilian Stüber <Maximilian.Stueber@me.com>
2015-10-16 16:53:46 +0200

    Changes to project structure

--- mdm_core/mdm_view/src/main/webapp/Component.js
+++ mdm_core/mdm_view/src/main/webapp/Component.js
@@ -2,46 +2,7 @@ jQuery.sap.declare("uni.mannheim.mdm.Component");
 
 sap.ui.core.UIComponent.extend("uni.mannheim.mdm.Component", {
 	metadata : {
-		routing : {
-			config : {
-				viewType : "XML",
-				viewPath : "uni.mannheim.mdm.view",
-				targetControl : "main",
-				targetAggregation : "pages",
-				clearTarget : false
-			},
-			routes : [ {
-				pattern : "",
-				name : "Overview",
-				view : "Overview"
-			}, {
-				pattern : "masterdata/overview",
-				name : "masterdata.Overview",
-				view : "masterdata.Overview"
-			}, {
-				pattern : "masterdata/customer/details",
-				name : "masterdata.CustomerDetails",
-				view : "masterdata.CustomerDetails"
-			}, {
-				pattern : "masterdata/customer/overview",
-				name : "masterdata.CustomerOverview",
-				view : "masterdata.CustomerOverview"
-			}, {
-				pattern : "import/overview",
-				name : "import.Overview",
-				view : "import.Overview"
-			},
-			{
-				pattern : "import/Uploader",
-				name : "import.Uploader",
-				view : "import.Uploader"
-			},
-			{
-				pattern : "import/FieldMapper",
-				name : "import.FieldMapper",
-				view : "import.FieldMapper"
-			},]
-		}
+		manifest : "json"
 	}
 });
 
@@ -63,14 +24,4 @@ uni.mannheim.mdm.Component.prototype.destroy = function() {
 	}
 
 	sap.ui.core.UIComponent.destroy.apply(this, arguments);
-};
-
-uni.mannheim.mdm.Component.prototype.createContent = function() {
-	this.view = sap.ui.view({
-		id : "app",
-		viewName : "uni.mannheim.mdm.view.App",
-		type : sap.ui.core.mvc.ViewType.XML
-	});
-
-	return this.view;
 };
--- mdm_core/mdm_view/src/main/webapp/controller/masterdata/CustomerOverview.controller.js
+++ mdm_core/mdm_view/src/main/webapp/controller/masterdata/CustomerOverview.controller.js
@@ -1,22 +1,4 @@
 sap.ui.controller("uni.mannheim.mdm.controller.masterdata.CustomerOverview", {
-
-	/**
-	 * Method onInit
-	 */
-	onInit : function() {
-		var sOrigin = window.location.protocol + "//"
-				+ window.location.hostname
-				+ (window.location.port ? ":" + window.location.port : "");
-
-		var customerListOdataServiceUrl = sOrigin
-				+ "/mdm_view/services/customer.svc";
-
-		var odataModel = new sap.ui.model.odata.ODataModel(
-				customerListOdataServiceUrl);
-
-		odataModel.setCountSupported(false);
-		this.getView().setModel(odataModel);
-	},
 	
 	/**
 	 * Method onNavTargetsObtained
