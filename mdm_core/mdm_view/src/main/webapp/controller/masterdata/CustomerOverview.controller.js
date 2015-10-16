cb500dbc90fc5d298cb50075679bbc86152d56c0
Maximilian Stüber <maximilian.stueber@me.com>
2015-10-17 00:04:45 +0200

    Changes to customer overview

--- mdm_core/mdm_view/src/main/webapp/controller/masterdata/CustomerOverview.controller.js
+++ mdm_core/mdm_view/src/main/webapp/controller/masterdata/CustomerOverview.controller.js
@@ -1,10 +1,51 @@
 sap.ui.controller("uni.mannheim.mdm.controller.masterdata.CustomerOverview", {
-	
+
+	/**
+	 * Method onInit
+	 */
+	onInit : function() {
+		
+		// To-Dd: Use model defined in Component.js
+		var model = new sap.ui.model.odata.ODataModel("/mdm_view/services/data.svc");
+		this.getView().setModel(model);
+		
+		// register event for selection
+		var table = this.getView().byId("customerTable");
+		table.setMode(sap.m.ListMode.SingleSelectMaster);
+		table.attachEvent("selectionChange", this.onSelectionChange, this);
+	},
+
 	/**
 	 * Method onNewCustomer
 	 */
-	onNewCustomer : function () {
+	onNewCustomer : function() {
 		var router = sap.ui.core.UIComponent.getRouterFor(this);
-		router.navTo("masterdata.CustomerDetails", false);
+		router.navTo("masterdata.CustomerCreate", false);
+	},
+	
+	/**
+	 * Method onSelectionChange
+	 */
+	onSelectionChange : function(oEvent) {
+		
+		// get Id
+		var id = oEvent.getParameter("listItem").getBindingContext().getProperty("Id");
+		
+		// deselect all rows
+		var table = this.getView().byId("customerTable");
+		table.removeSelections(true);
+		
+		// navigate to details
+		var router = sap.ui.core.UIComponent.getRouterFor(this);
+		router.navTo("masterdata.CustomerDetails", {id: id}, false);
+		
+	},
+	
+	/**
+	 * Method onRefresh
+	 */
+	onRefresh : function() {
+		var model = this.getView().getModel();
+		model.refresh(true);
 	}
 });
