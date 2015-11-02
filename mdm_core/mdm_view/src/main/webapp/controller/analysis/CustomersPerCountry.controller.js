c02bd2f4cb2a0a628b32c2b020b9c9afadcb2ffa
Eva <eva@gje.ch>
2015-11-02 11:07:37 +0100

    Analytic Map: Initial setups

--- mdm_core/mdm_view/src/main/webapp/controller/analysis/CustomersPerCountry.controller.js
+++ mdm_core/mdm_view/src/main/webapp/controller/analysis/CustomersPerCountry.controller.js
@@ -1,5 +1,50 @@
-sap.ui.controller("uni.mannheim.mdm.controller.analysis.CustomersPerCountry", {
+//sap.ui.controller("uni.mannheim.mdm.controller.analysis.CustomersPerCountry", {
 	
-	
+	sap.ui.define(['sap/ui/vbm/AnalyticMap'],
+			function(AnalyticMap) {
+			"use strict";
 
-});
+			//AnalyticMap.GeoJSONURL  =  "test-resources/sap/ui/vbm/demokit/media/analyticmap/continent.json";
+			AnalyticMap.GeoJSONURL  =  "src/main/webapp/resources/analyticmap/L0.json";
+
+			sap.ui.controller("uni.mannheim.mdm.controller.analysis.CustomersPerCountry", {
+			
+				onRegionClick: function (e)
+				{
+					sap.m.MessageToast.show( "onRegionClick " + e.getParameter( "code" ) );
+				},
+
+				onRegionContextMenu: function ( e )
+				{
+					sap.m.MessageToast.show( "onRegionContextMenu " + e.getParameter( "code" ) );
+				},
+			
+				onAddItem: function (evt)
+				{
+					var item = new sap.ui.vbm.Region({ code: 'SA', color: 'rgba(198,225,125,1.0)', tooltip: 'South America'});
+					this.byId("vbi").insertRegion(item, 0);
+				
+					this.byId("AddButton").setEnabled(false);
+					this.byId("RemoveAllButton").setEnabled(true);
+				},
+			
+				onRemoveAllItems: function (evt)
+				{	
+					this.byId("vbi").removeAllRegions();
+
+					this.byId("AddButton").setEnabled(true);
+					this.byId("RemoveAllButton").setEnabled(false);
+				},
+			
+				onZoomIn : function() 
+				{
+					this.byId("vbi").zoomToRegions( ["SA"] );		
+					this.byId("ZoomOut").setEnabled(true);
+					this.byId("ZoomIn").setEnabled(false);
+				}
+			});
+
+
+		}, /* bExport= */ true);	
+
+//});
