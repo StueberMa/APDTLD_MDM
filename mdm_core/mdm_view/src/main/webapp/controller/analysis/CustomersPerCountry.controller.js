caa6825bcbc65ac4c9470d45bffcec1e1ce59462
Maximilian Stüber <maximilian.stueber@me.com>
2015-11-03 12:39:16 +0100

    Enhanced Analysis

--- mdm_core/mdm_view/src/main/webapp/controller/analysis/CustomersPerCountry.controller.js
+++ mdm_core/mdm_view/src/main/webapp/controller/analysis/CustomersPerCountry.controller.js
@@ -16,6 +16,40 @@ sap.ui.define([
 			onInit : function() {
 				var model = new sap.ui.model.json.JSONModel("./services/analysis/customer/perCountry");
 				this.getView().setModel(model, "perCountry")
+			},
+			
+			/**
+			 * Method formatCountry
+			 */
+			formatCountry : function(status) {	
+				
+				if(status == "AT")
+					return "Austria";
+					
+				if(status == "BL")
+					return "Belgium";
+				
+				if(status == "CN")
+					return "China";
+				
+				if(status == "FR")
+					return "France";
+				
+				if(status == "DE")
+					return "Germany";
+				
+				if(status == "GB")
+					return "Great Britain";
+				
+				if(status == "IT")
+					return "Italy";
+				
+				if(status == "NL")
+					return "Netherland";
+				
+				if(status == "US")
+					return "United State";
 			}
+			
 		});
 	}, true);
